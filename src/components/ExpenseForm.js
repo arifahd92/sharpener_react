54ddac5f86e754da96a7ea58da4113450679dcb3
md arif <arifahd92@gmail.com>
2023-06-04 11:59:08 +0530

    merged in single state and consoling value on submitting

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,26 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function ExpenseWithInput() {
+  const [inputState, setInputState] = useState({
+    title: "",
+    amount: 0,
+    location: "",
+  });
+  function handleSubmit(e) {
+    console.log(e.preventDefault());
+    console.log("form submitted");
+    console.log(inputState);
+  }
   return (
     <>
-      <form action="#">
+      <form action="#" onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="title"
-          onChange={(e) => console.log(e.target.value)}
+          onChange={(e) =>
+            setInputState({ ...inputState, title: e.target.value })
+          }
         />
         <br />
         <input
           type="text"
           placeholder="amount"
-          onChange={(e) => console.log(e.target.value)}
+          onChange={(e) =>
+            setInputState({ ...inputState, amount: e.target.value })
+          }
         />
         <br />
         <input
           type="text"
           placeholder="location"
-          onChange={(e) => console.log(e.target.value)}
+          onChange={(e) =>
+            setInputState({ ...inputState, location: e.target.value })
+          }
         />
+        <br />
+        <input type="submit" />
       </form>
     </>
   );
