d68f5c7e3958105ce7765412829479a97ab69a81
md arif <arifahd92@gmail.com>
2023-06-04 11:10:22 +0530

    update expenses

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import "./expenses.css";
 
 const Expenses = ({ cindex, name, amount, location, delFunc }) => {
   console.log(name);
   console.log(cindex);
+  const [prevName, setPrevName] = useState(name);
+  const [prevAmount, setPrevAmount] = useState(amount);
+  const updateExpenses = () => {
+    setPrevName("updated");
+    setPrevAmount(100);
+  };
   return (
     <div className="maincont">
       <div className="cont-item">
-        <div className="name">{name + " " + cindex}</div>
-        <div className="price">{amount}</div>
+        <div className="name">{prevName + " " + cindex}</div>
+        <div className="price">{prevAmount + "$"}</div>
         <div className="location">{location}</div>
         <button onClick={() => delFunc(cindex)}> delete</button>
+        <button onClick={updateExpenses}>update</button>
       </div>
     </div>
   );
